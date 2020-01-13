d9b2b257fc14cdeedd0a796a1e0c68740cc1e0d5
Шайнурова Екатерина <shainurova.ekaterina@MacBook-Pro-Sajnurova.local>
2020-01-13 13:09:52 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+
+	// creates a new file watcher
+	fmt.Printf("Watcher has been started....")
+
+	watcher, err := fsnotify.NewWatcher()
+
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			// watch for events
+			case event := <-watcher.Events:
+				fmt.Printf("EVENT! %#v\n", event)
+
+				// watch for errors
+			case err := <-watcher.Errors:
+				fmt.Println("ERROR", err)
+			}
+		}
+	}()
+
+	// out of the box fsnotify can watch a single file, or a single directory
+	if err := watcher.Add("/Users/shainurova.ekaterina/test-file-watcher"); err != nil {
+		fmt.Println("ERROR", err)
+	}
+
+	<-done
+}
